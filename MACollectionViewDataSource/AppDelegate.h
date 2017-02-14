625f814878680a111d04de5a00e5d34e0ed9f810
minerany <minerany@foxmail.com>
2017-02-14 15:19:10 +0800

    Initial Commit

---
+++ MACollectionViewDataSource/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MACollectionViewDataSource
+//
+//  Created by miner on 2017/2/14.
+//  Copyright © 2017年 miner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
